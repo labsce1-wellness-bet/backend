34c53c51a095a250b514d3ab40b2d7af51afe193
bmakau <bonifacemtk15@yahoo.com>
2019-06-03 14:36:09 -0400

    user model done

---
+++ src/main/java/com/lambdaschool/wellness/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.lambdaschool.wellness.controllers;
+
+
+import com.lambdaschool.wellness.model.User;
+import com.lambdaschool.wellness.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+//@CrossOrigin("http://localhost:3000")
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result)
+    {
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error:result.getFieldErrors()){
+
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        User newUser = userService.saveOrUpdateProduct(user);
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public Iterable<User>getAllUsers(){
+        return userService.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+
+
+
+}
---
+++ src/main/java/com/lambdaschool/wellness/model/User.java
@@ -0,0 +1,71 @@
+package com.lambdaschool.wellness.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @JsonIgnore
+    private String fname;
+
+    @JsonIgnore
+    private String lname;
+
+    private String fullname;
+
+    private String email;
+
+    public User()
+    {
+        //default constructor
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getFname()
+    {
+        return fname;
+    }
+
+    public void setFname(String fname)
+    {
+        this.fname = fname;
+    }
+
+    public String getLname()
+    {
+        return lname;
+    }
+
+    public void setLname(String lname)
+    {
+        this.lname = lname;
+    }
+
+    public String getFullname()
+    {
+        return lname + " " + fname;
+    }
+
+
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
