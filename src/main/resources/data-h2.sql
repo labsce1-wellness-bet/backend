9a6c9c508cfdd0f61eba616b89a88379f6f7f8a7
ckirkwood94 <ckirkwood94@gmail.com>
2019-06-04 18:01:32 -0400

    Basic user seeds

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,9 @@
-INSERT INTO users
-  (fname, lname, email)
+INSERT INTO User
+  (id, fname, lname, email)
 VALUES
-  ('Caleb', 'Kirkwood', 'ckirkwood@example.com'),
-  ('Admin', 'Istrator', 'admin@example.com');
+  (1, 'Caleb', 'Kirkwood', 'ckirkwood@example.com'),
+  (2, 'Admin', 'Istrator', 'admin@example.com'),
+  (3, 'Simon', 'Davis', 'sdavis@example.com'),
+  (4, 'Boniface', 'Makau', 'bmakau@example.com'),
+  (5, 'Akshay', 'Gadkari', 'agadkari@example.com'),
+  (6, 'Il Ho', 'Kim', 'ikim@example.com');
